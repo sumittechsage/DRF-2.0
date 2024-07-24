46ef27fdaba349ea115bf18c5d7a13fce84e9da5
gamersumit <saggarwal@innow8apps.com>
2024-07-24 18:37:49 +0530

    get set cookie code

--- cookies/urls.py
+++ cookies/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('set/', views.setcookie, name = "set-cookie"),
+    path('get/', views.getcookie, name = "get-cookie")
+]
--- cookies/views.py
+++ cookies/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 
 # Create your views here.
+def setcookie(request):
+    respone = Response({"message" : "cokkie set"}, status = 200)
+    respone.set_cookie('name', 'santa')
+    return 
+
+
+def getcookie(request):
+    cookies = request.COOKIES
+    name = cookies['name']
+    print("cookiees =====>", cookies)
+    print("name from cookiee =====>", name)
+    return Response({"message" : "cokkie read"}, status = 200)
