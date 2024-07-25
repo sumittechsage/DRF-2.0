cdbb152f938ab09256a71624f66e35ea06fb38d5
gamersumit <saggarwal@innow8apps.com>
2024-07-25 12:56:27 +0530

    session methods

--- session/urls.py
+++ session/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('set/', views.setsession, name = "set-session"),
+    path('setdefault/', views.setdefualtsession, name = "setdeafult-session"),
+    path('get/', views.getsession, name = "get-session"),
+    path('delete/', views.deletesession, name = "delete-session"),
+    path('flush/', views.flushsession, name = "flush-session"),
 ]
--- session/views.py
+++ session/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
+def setsession(request):
+    request.session['name'] = 'Session_name'
+    return JsonResponse({"message" : "session set"}, status = 200)
+
+
+def setdefualtsession(request):
+    ''' DICTIONARY METHOD "setdefault(key, value)" : if a key exsists return its value other wise insert key with value given.'''
+    age = request.session.setdefault('age', '26')
+    print("age ====>", age)
+    return JsonResponse({"message" : "session default set"}, status = 200)
+
+
+def getsession(request):
+    session= request.session
+    name = session['name']
+    print("session =====>", session)
+    print("name from session =====>", name)
+    return JsonResponse({"message" : "session read"}, status = 200)
+
+def deletesession(request):
+    ''' DELETES DATA FROM SESSION NOT THE SESSION ITSELF'''
+    session = request.session
+    name = session['name']
+    print("cookiees =====>", session)
+    print("name from cookiee =====>", name)
+    del session['name']
+    return JsonResponse({"message" : "session deleted"}, status = 200)
+  
+
+def flushsession(request):
+    ''' DELETE THE DATA AND COOKIES/SESSION : E.G :- LOGOUT FUNCTION'''
+    session = request.session
+    session.flush()
+    return JsonResponse({"message" : "session flushed"}, status = 200)
