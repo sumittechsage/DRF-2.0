be86894971ad83fc91b42acf9bf328f6747c320d
gamersumit <saggarwal@innow8apps.com>
2024-07-29 11:01:43 +0530

    function for model manager

--- manager/managers.py
+++ manager/managers.py
@@ -3,3 +3,6 @@ from django.db import models
 class CustomManager(models.Manager):
     def get_queryset(self):   # overriding Built-in method called when we call all()
         return super().get_queryset().order_by('name')  # now the base queryset is overrided with base + ordered by name = all.order_by('name')
+    
+    def get_stu_roll_range(self, r1, r2):  # creating a new method to get queryset in a range of roll numbers
+        return super().get_queryset().filter(roll__range=(r1, r2)) 
--- manager/views.py
+++ manager/views.py
@@ -5,8 +5,10 @@ from .models import Student
 def list_student_records(request):
     student_data_1 = Student.vidhyarthi.all()  # vidhyarhi is student model manager name
     student_data_2 = Student.students.all()  # students is another student model manager name
+    student_data_3 = Student.students.get_stu_roll_range(3,9)  # students is another student model manager name
     print("student data 1 ====>", student_data_1)
     print("student data 2 ====>", student_data_2)
+    print("student data 3 ====>", student_data_3)
     return JsonResponse({"message" : "request successful"}, status = 200)
 
 
