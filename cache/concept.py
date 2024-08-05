8adabaafd925a8ee44c5a3ef93178ba0734fd8ba
sumit <sumitaggarwal120220022@gmail.com>
2024-08-05 17:39:51 +0530

    started concept of cache

---
+++ cache/concept.py
@@ -0,0 +1,19 @@
+
+
+
+'''
+                    C A C H E
+        --------------------------------------
+=>  A Cache is an information technology for the temporary storage (caching) of Web documents, such as Web Pages, images and other types of Web multimedia, to reduce server lag.
+
+=> Caching is one of those methods which a website implements to become faster. It is cost efficient and saves CPU Processing time.
+
+=> Django comes with a robust cache system that lets you save dynamic pages so they don't have to be calculated for each request.
+
+=> You can cache output of specific views, you can cache only the pieces that are difficult to produce, or you can cache your entire site.
+
+## FOLLOWING ARE THE OPTIONS OF CACHING :-
+    1. Database Caching
+    2. File System Caching 
+    3. Local Memory Caching
+'''
